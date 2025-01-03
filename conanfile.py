7aa706430d32dbec3f26d63c44a96b2e4df1a93c
Leonardo Parente <23251360+leoparente@users.noreply.github.com>
2025-01-03 10:19:51 -0300

    chore: update 3rd party dependencies

--- conanfile.py
+++ conanfile.py
@@ -6,18 +6,18 @@ class Pktvisor(ConanFile):
     generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
-        self.requires("catch2/3.6.0")
+        self.requires("catch2/3.7.1")
         self.requires("corrade/2020.06")
-        self.requires("cpp-httplib/0.16.0")
+        self.requires("cpp-httplib/0.17.3")
         self.requires("docopt.cpp/0.6.3")
         self.requires("fast-cpp-csv-parser/cci.20240102")
         self.requires("json-schema-validator/2.3.0")
         self.requires("libmaxminddb/1.10.0")
         self.requires("nlohmann_json/3.11.3")
-        self.requires("openssl/3.3.1")
+        self.requires("openssl/3.3.2")
         if self.settings.os != "Windows":
             self.requires("libpcap/1.10.4", force=True)
-        self.requires("opentelemetry-proto/1.3.0")
+        self.requires("opentelemetry-cpp/1.14.2")
         self.requires("pcapplusplus/23.09")
         self.requires("abseil/20240116.2", force=True)
         self.requires("protobuf/5.27.0")
@@ -26,7 +26,7 @@ class Pktvisor(ConanFile):
         self.requires("uvw/3.4.0")
         self.requires("yaml-cpp/0.8.0")
         self.requires("robin-hood-hashing/3.11.5")
-        self.requires("libcurl/8.9.1")
+        self.requires("libcurl/8.10.1")
         self.requires("sentry-crashpad/0.6.5")
 
     def build_requirements(self):
