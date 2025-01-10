7a781d160a47f52d9f7b5145de87c4e5e6a0f54c
Leonardo Parente <23251360+leoparente@users.noreply.github.com>
2025-01-10 11:55:53 -0300

    fix conanfile

--- conanfile.py
+++ conanfile.py
@@ -28,8 +28,11 @@ class Pktvisor(ConanFile):
         self.requires("yaml-cpp/0.8.0")
         self.requires("robin-hood-hashing/3.11.5")
         self.requires("libcurl/8.11.1")
-        self.requires("libevent/2.1.12")
-        if self.settings.compiler.libc and self.settings.compiler.libc != "musl":
+        if (
+            self.settings.compiler
+            and self.settings.compiler.libc
+            and self.settings.compiler.libc != "musl"
+        ):
             self.requires("sentry-crashpad/0.6.5")
 
     def build_requirements(self):
