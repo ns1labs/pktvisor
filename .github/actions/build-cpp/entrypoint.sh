e706986363cf420511183c5a2df0f9f24210045b
Leonardo Parente <23251360+leoparente@users.noreply.github.com>
2025-01-17 21:07:53 -0300

    small fix

--- .github/actions/build-cpp/entrypoint.sh
+++ .github/actions/build-cpp/entrypoint.sh
@@ -23,10 +23,10 @@ function build() {
   cd /pktvisor-src/
   conan profile detect -f
   cd /pktvisor-src/build/
-  if [[ "$INPUT_ARCH" == "amd64"]]; then
+  if [ "$INPUT_ARCH" == "amd64"]; then
     PKG_CONFIG_PATH=/local/lib/pkgconfig cmake .. -DCMAKE_BUILD_TYPE=$INPUT_BUILD_TYPE \
      -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=./cmake/conan_provider.cmake -DASAN=$INPUT_ASAN
-  elif [[ "$INPUT_ARCH" == "arm64" ]]; then
+  elif [ "$INPUT_ARCH" == "arm64" ]; then
     echo "[settings]
     os=Linux
     arch=armv8
